f58ff77d5bfcd26d242d3f9adcb04df44deed6f8
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-23 18:48:28 -0500

    Commit

--- script/helloworld.js
+++ script/helloworld.js
@@ -1,4 +1,23 @@
-let map;
+let map1;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+let map2;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+
+let map3;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
