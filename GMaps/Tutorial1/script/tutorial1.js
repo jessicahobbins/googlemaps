dbd9d8a3ba59f4b32a5b156ef7e28dce39eec00d
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-24 14:39:24 -0500

    Adjusting map zoom

--- GMaps/Tutorial1/script/tutorial1.js
+++ GMaps/Tutorial1/script/tutorial1.js
@@ -4,7 +4,7 @@ function initMap() {
     const TrentUniversity = { lat: 44.363410789383174, lng: -78.29052212456627 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
+      zoom: 11,
       center: TrentUniversity,
     });
     // The marker, positioned at Uluru
