14f1f877302f16469eab33f8854a7c3eb8b117df
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-24 14:08:44 -0500

    Tutorial 1

--- GMaps/Tutorial1/script/tutorial1.js
+++ GMaps/Tutorial1/script/tutorial1.js
@@ -4,7 +4,7 @@ function initMap() {
     const TrentUniversity = { lat: 44.363165340797, lng: -78.29052212456627 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 8,
       center: TrentUniversity,
     });
     // The marker, positioned at Uluru
