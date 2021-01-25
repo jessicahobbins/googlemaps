951ca1493c7af941a639d9f06ebef391d7f415df
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-25 13:11:45 -0500

    Changing map zoom

--- JavaScriptAPI/script/helloworld.js
+++ JavaScriptAPI/script/helloworld.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 46.157257770401074, lng: -84.73896412905184 },
-    zoom: 8,
+    zoom: 6,
     mapTypeId: 'satellite'
   });
 }
