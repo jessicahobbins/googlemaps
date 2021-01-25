2d3f327f64ac54829d8f3075fc6c26066715673f
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-25 13:38:53 -0500

    Test

--- JavaScriptAPI/script/helloworld.js
+++ JavaScriptAPI/script/helloworld.js
@@ -6,11 +6,4 @@ function initMap() {
     zoom: 6,
     mapTypeId: 'satellite'
   });
-  mapLabel = new MapLabel({
-    text: 'Lake Ontario',
-    position: new google.maps.LatLng(43.684027511300016, -77.81757779934962),
-    map: map,
-    fontSize: 35,
-    align: 'right'
-});
 }
