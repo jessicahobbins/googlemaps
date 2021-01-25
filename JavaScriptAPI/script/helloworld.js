993c07e5ee4f8acaf961d78780c5a036b7a7adf1
Jessica Hobbins <jessica.hobbins@flemingcollege.ca>
2021-01-25 13:07:40 -0500

    Editing map type

--- JavaScriptAPI/script/helloworld.js
+++ JavaScriptAPI/script/helloworld.js
@@ -4,5 +4,6 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 26.028473, lng: -80.114671 },
     zoom: 10,
+    mapTypeId: 'satellite'
   });
 }
